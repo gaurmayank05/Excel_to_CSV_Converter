6dacc72a9533950955f2fa3823d71ea3883ac173
Anmol Goyal <06anmol6@gmail.com>
2024-07-21 22:43:01 +0530

    Handle Multiple Exceptions which can be occur  like single blank row ,multiple blank row ,spaces in existing column and many more exception.

--- src/main/java/org/example/ExcelToCSV.java
+++ src/main/java/org/example/ExcelToCSV.java
@@ -5,7 +5,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,13 +19,14 @@ public class ExcelToCSV {
      * @throws Exception if an error occurs during the conversion process
      */
     public void ExcelToCSVConverter(String configurableExcel, String inputExcel) throws Exception {
+        ZipDirectory zipDirectory = new ZipDirectory();
+        String tempFolder = zipDirectory.createTempDirectory("tempCSV");
+        String zipDestinationFolder = "D://CSV.zip";
         ConfigurableExcel excelQueryParameters = new ConfigurableExcel(0, -1, 1, -1, null, null, false, true, null, false);
-
         InputStream configurableExcelPath = getResourceAsStream(configurableExcel);
-
         List<List<String>> excelConfigurationList = queryExcelData(configurableExcelPath, excelQueryParameters);
         List<ConfigurableExcel> queryConfigList = fillSheetParameter(excelConfigurationList);
-        //validateSheetAndPath(excelQueryParameters,queryConfigList);
+        validateSheetAndPath(queryConfigList, excelConfigurationList,inputExcel);
         for (ConfigurableExcel parameters : queryConfigList) {
             List<List<String>> excelData;
             InputStream inputExcelPath = getResourceAsStream(inputExcel);
@@ -38,22 +41,33 @@ public class ExcelToCSV {
             if (parameters.isTranspose()) {
                 excelData = transposeData(excelData);
             }
-            writeCSV(parameters, excelData);
+            String csvFilePath = createDirectory(tempFolder, parameters);
+            writeCSV(parameters, excelData, csvFilePath);
+            inputExcelPath.close();
         }
+        zipDirectory.zipFolder(tempFolder, zipDestinationFolder);
+        zipDirectory.deleteTempDirectory(tempFolder);
+        configurableExcelPath.close();
     }
-
-
-    private void validateSheetAndPath(List<ConfigurableExcel> queryConfigList, List<List<String>> configExcelList) throws Exception {
+    /**
+     * Validates the sheet names and paths in the configuration.
+     * Throws exceptions if any inconsistencies are found.
+     *
+     * @param queryConfigList    The list of configurable Excel parameters.
+     * @param configExcelList    The configuration data from the Excel file.
+     * @throws Exception If any validation fails.
+     */
+    private void validateSheetAndPath(List<ConfigurableExcel> queryConfigList, List<List<String>> configExcelList, String inputExcel) throws Exception {
         for (List<String> rowData : configExcelList) {
-            if (rowData.stream().allMatch(String::isEmpty)) {
+            if (rowData.stream().allMatch(cellData -> cellData.trim().isEmpty())) {
                 throw new Exception("CONFIGURABLE EXCEL SHEET CONTAINS BLANK ROWS");
             }
         }
         for (ConfigurableExcel parameters : queryConfigList) {
-            boolean isSheetNameEmpty = parameters.getSheetName() == null || parameters.getSheetName().isEmpty();
-            boolean isSheetPathEmpty = parameters.getSheetPath() == null || parameters.getSheetPath().isEmpty();
+            boolean isSheetNameEmpty = parameters.getSheetName() == null || parameters.getSheetName().trim().isEmpty();
+            boolean isSheetPathEmpty = parameters.getSheetPath() == null || parameters.getSheetPath().trim().isEmpty();
             if (isSheetNameEmpty && !isSheetPathEmpty) {
-                throw new Exception("CSD SHEET DOES NOT EXIST BUT CSV DIRECTORY PATH EXISTS:" + parameters.getSheetPath());
+                throw new Exception("CSD SHEET DOES NOT EXIST BUT CSV DIRECTORY PATH EXISTS: " + parameters.getSheetPath());
             }
             if (!isSheetNameEmpty && isSheetPathEmpty) {
                 throw new Exception(parameters.getSheetName() + " CSD SHEET EXISTS BUT CSV DIRECTORY PATH DOES NOT EXIST");
@@ -62,32 +76,115 @@ public class ExcelToCSV {
                 throw new Exception("CSD SHEET AND CSV DIRECTORY PATH DOES NOT EXIST");
             }
         }
+        Map<String, List<Integer>> multipleBlankRowsMap = new HashMap<>();
+        Map<String, Integer> singleBlankRowMap = new HashMap<>();
+        List<String> whitespaceErrors = new ArrayList<>();
+        List<String> blankRowWhitespaceErrors = new ArrayList<>();
+        try (InputStream inputExcelPath = getResourceAsStream(inputExcel); Workbook workbook = new XSSFWorkbook(inputExcelPath)) {
+            for (ConfigurableExcel parameters : queryConfigList) {
+                String sheetName = parameters.getSheetName();
+                Sheet sheet = workbook.getSheet(sheetName);
+                if (sheet == null) {
+                    throw new Exception("SHEET DOES NOT EXIST: " + sheetName);
+                }
+                for (int rowIndex = 0; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                    Row row = sheet.getRow(rowIndex);
+                    if (row != null) {
+                        boolean isRowBlank = true;
+                        boolean hasWhitespaceInBlankCell = false;
+                        for (int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
+                            Cell cell = row.getCell(cellIndex);
+                            if (cell != null) {
+                                String cellValue = cell.toString();
+                                if (cellValue.trim().isEmpty() && !cellValue.isEmpty()) {
+                                    hasWhitespaceInBlankCell = true;
+                                    whitespaceErrors.add("Whitespace in Sheet: " + sheetName + " at Row: " + (rowIndex + 1) + " Column: " + (cellIndex + 1));
+                                }
+                                if (cell.getCellType() != CellType.BLANK && !cellValue.trim().isEmpty()) {
+                                    isRowBlank = false;
+                                }
+                            }
+                        }
+                        if (isRowBlank) {
+                            if (multipleBlankRowsMap.containsKey(sheetName)) {
+                                multipleBlankRowsMap.get(sheetName).add(rowIndex + 1);
+                            } else if (singleBlankRowMap.containsKey(sheetName)) {
+                                List<Integer> rowList = new ArrayList<>();
+                                rowList.add(singleBlankRowMap.remove(sheetName));
+                                rowList.add(rowIndex + 1);
+                                multipleBlankRowsMap.put(sheetName, rowList);
+                            } else {
+                                singleBlankRowMap.put(sheetName, rowIndex + 1);
+                            }
+                            if (hasWhitespaceInBlankCell) {
+                                blankRowWhitespaceErrors.add("Whitespace in blank row in Sheet: " + sheetName + " at Row: " + (rowIndex + 1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if (!multipleBlankRowsMap.isEmpty()) {
+            sb.append("Multiple Blank Rows: ");
+            for (Map.Entry<String, List<Integer>> entry : multipleBlankRowsMap.entrySet()) {
+                sb.append("\nSheet: ").append(entry.getKey()).append(" Rows: ").append(entry.getValue());
+            }
+            sb.append("\n");
+        }
+        if (!singleBlankRowMap.isEmpty()) {
+            if (!sb.isEmpty()) {
+                sb.append("\n");
+            }
+            sb.append("Single Blank Row: ");
+            for (Map.Entry<String, Integer> entry : singleBlankRowMap.entrySet()) {
+                sb.append("\nSheet: ").append(entry.getKey()).append(" Row: ").append(entry.getValue());
+            }
+            sb.append("\n");
+        }
+        if (!whitespaceErrors.isEmpty()) {
+            if (!sb.isEmpty()) {
+                sb.append("\n");
+            }
+            sb.append("Whitespace errors found in existing rows: ");
+            for (String error : whitespaceErrors) {
+                sb.append("\n").append(error);
+            }
+            sb.append("\n");
+        }
+        if (!blankRowWhitespaceErrors.isEmpty()) {
+            if (!sb.isEmpty()) {
+                sb.append("\n");
+            }
+            sb.append("Whitespace errors found in blank rows: ");
+            for (String error : blankRowWhitespaceErrors) {
+                sb.append("\n").append(error);
+            }
+        }
+        if (!sb.isEmpty()) {
+            throw new Exception(sb.toString());
+        }
     }
+
     /**
      * Queries the data from an Excel file based on the provided parameters.
      *
      * @param getExcelPath the InputStream of the Excel files either configurable Excel file or data Excel file
      * @param parameters the configurable Excel parameters for querying the data
      * @return a list of lists, where each inner list represents a row of data from the Excel file
-     * @throws IOException if an error occurs while reading the Excel file
      */
-
-    private List<List<String>> queryExcelData(InputStream getExcelPath, ConfigurableExcel parameters) throws IOException {
+    private List<List<String>> queryExcelData(InputStream getExcelPath, ConfigurableExcel parameters){
         List<List<String>> excelData = new ArrayList<>();
-        try {
-            Workbook workbook = new XSSFWorkbook(getExcelPath);
-
-            // Get the sheet to be processed based on the provided parameters
+        try (getExcelPath; Workbook workbook = new XSSFWorkbook(getExcelPath)) {
             Sheet sheet = getSheet(workbook, parameters);
-            if(parameters.isTranspose() && (parameters.getSheetRange().isEmpty() || parameters.getSheetRange()==null))
-            {
+            if (parameters.isTranspose() && (parameters.getSheetRange().isEmpty() || parameters.getSheetRange() == null)) {
                 parameters.setStartRow(2);
             }
-            if (parameters.getEndRow()==-1){
+            if (parameters.getEndRow() == -1) {
                 parameters.setEndRow(sheet.getLastRowNum());
             }
-            if (parameters.getEndColumn()==-1){
-                parameters.setEndColumn(maxColumn(sheet,parameters));
+            if (parameters.getEndColumn() == -1) {
+                parameters.setEndColumn(maxColumn(sheet, parameters));
             }
             if (parameters.isComment()) {
                 parameters.setEndColumn(parameters.getEndColumn() + 1);
@@ -105,7 +202,8 @@ public class ExcelToCSV {
                 }
                 excelData.add(rowData);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
         return excelData;
@@ -176,7 +274,7 @@ public class ExcelToCSV {
         }
         for (int j = 0; j < colCount; j++) {
             if (j == 0) addDeleteColumn.add("deleted");
-             else addDeleteColumn.add("False");
+            else addDeleteColumn.add("False");
         }
         return addDeleteColumn;
     }
@@ -203,12 +301,12 @@ public class ExcelToCSV {
      *
      * @param parameters The configurableExcel object containing parameters for particular sheet.
      * @param excelData the data to be written to the CSV file
+     * @param csvFilePath is used to store the path of temporary folder
      * @throws IOException if an error occurs while writing the CSV file
      */
-    private void writeCSV(ConfigurableExcel parameters, List<List<String>> excelData) throws IOException {
+    private void writeCSV(ConfigurableExcel parameters, List<List<String>> excelData, String csvFilePath) throws IOException {
 
         if (parameters.getSheetPath() != null) {
-            String csvFilePath = createDirectory(parameters);
             try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFilePath), StandardCharsets.UTF_8))) {
                 // Write the standardized header to the CSV file
                 standardizedHeader(writer, excelData);
@@ -219,7 +317,7 @@ public class ExcelToCSV {
                         else writer.append("");
                         if (i < row.size() - 1) writer.append(",");
                     }
-                    writer.newLine();
+                    if (rowIndex != excelData.size()-1) writer.newLine();
                 }
             }
         }
@@ -228,18 +326,19 @@ public class ExcelToCSV {
     /**
      * Creates a directory based on the provided configurableExcel parameter's sheet path.
      *
+     * @param destinationFolder is used to store the path of destinationFolder
+     *                          ( for below method it's used to store the path of temporary folder)
      * @param parameter The configurableExcel object containing sheet information.
      * @return The absolute path of the directory where the file will be saved,
      *         or an empty string if the sheet path is null.
      */
-    private String createDirectory(ConfigurableExcel parameter) {
-        String outputDirectory = "D://";
+    private String createDirectory(String destinationFolder, ConfigurableExcel parameter) {
         String absolutePath = "";
         if (parameter.getSheetPath() != null) {
             File file = new File(parameter.getSheetPath());
             String directoryPath = file.getParent();
             String csvFile = file.getName();
-            File folder = new File(outputDirectory + File.separator + directoryPath);
+            File folder = new File(destinationFolder + File.separator + directoryPath);
             if (!folder.exists()) {
                 //noinspection ResultOfMethodCallIgnored
                 folder.mkdirs();
@@ -303,6 +402,7 @@ public class ExcelToCSV {
             List<List<String>> tempExcelData = queryExcelData(inputExcelPath, parameters);
             if (excelData == null) excelData = tempExcelData;
             else excelData.addAll(tempExcelData);
+            inputExcelPath.close();
         }
         return excelData;
     }
